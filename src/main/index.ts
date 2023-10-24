7b3d91bc8592ab3e682d2048c26982f01e56d994
Daniel Goncalves <danielopesgl05@gmail.com>
2023-10-24 11:39:53 -0300

    build: add eslint and jest libs

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,9 @@
 class Person {
-  execute(): string {
-    return "teste"
+  execute (): string {
+    return 'teste'
   }
 }
+
+const person = new Person()
+
+console.log('🚀 ~ file: index.ts:10 ~ person.execute():', person.execute())
